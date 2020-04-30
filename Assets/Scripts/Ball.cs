bfea3795c5b14e3c4354055ebff8bf81e18ddb61
Robert May <robertmay@Roberts-iMac.local>
2020-04-30 14:56:20 -0400

    First commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Ball : MonoBehaviour
+{
+    public ParticleSystem collisionParticle;
+    
+    private SpriteRenderer _renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+
+        Color color = Color.HSVToRGB(Random.value, 0.4f, 1f);
+        _renderer.color = color;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        ParticleSystem p = Instantiate(collisionParticle, other.contacts[0].point, Quaternion.identity);
+        p.startColor = _renderer.color;
+        p.transform.localScale *= other.contacts[0].normalImpulse / 25f;
+        
+        Destroy(p.gameObject, p.main.duration);
+    }
+}
---
+++ Assets/Scripts/BallSummoner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSummoner : MonoBehaviour
+{
+    [Header("Balls")]
+    public GameObject ball;
+    public float ballLifetime;
+
+    [Space]
+    [Header("Spawning")]
+    public int spawnCount;
+    public float spawnDelay;
+
+    private Camera _mainCamera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(Spawn(spawnCount));
+        }
+    }
+
+    IEnumerator Spawn(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Spawn();
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+    void Spawn()
+    {
+        Vector3 position = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        position.z = 0;
+
+        GameObject spawned = Instantiate(
+            ball,
+            position,
+            Quaternion.identity
+        );
+        
+        Destroy(spawned, ballLifetime);
+    }
+}
